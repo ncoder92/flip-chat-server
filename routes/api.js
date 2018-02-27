7f5fb77b84f80180f07a261715da69073d8e1720
Filipe Rainho <filipenrainho@gmail.com>
2018-02-27 15:12:58 +0100

    Create update room api route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const Room = require('../models/room');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.statusMessage = 'Yep, I\'m an API';
   res.status(209).json({ hi: 'Welcome to flipChat' });
+});
+
+router.post('/:room/update', (req, res, next) => {
+  const roomId = req.params.room;
+  const newMessage = req.body.message;
+  Room.findOneAndUpdate({ code: roomId }, { $push: { messages: newMessage } }, { new: true })
+    .then((roomFound) => {
+      console.log(roomFound);
+    })
+    .catch(next);
 });
 
 module.exports = router;
