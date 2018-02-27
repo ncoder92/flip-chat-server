ef535f4d6d04b4958ec2983c43200bc656c0cec6
Filipe Rainho <filipenrainho@gmail.com>
2018-02-27 15:57:22 +0100

    Add manually join specific room

--- socket/io.js
+++ socket/io.js
@@ -3,9 +3,10 @@ const io = require('socket.io')();
 /* GET home page. */
 io.on('connection', (socket) => {
   console.log('A user connected');
+  socket.join('255699');
 
   socket.on('sending-message', (message) => {
-    socket.emit('new-message', message);
+    socket.to('255699', message);
   });
 });
 
